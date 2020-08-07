0c1dc7d9e033106349f9fa0e67d567db054dc24e
André Alves <aalves@MacBook-Pro.local>
2020-08-07 10:53:38 -0300

    update

---
+++ preview.py
@@ -0,0 +1,16 @@
+a=1
+b=2
+lista1 = [1,2,3]
+(a,b) = (b,a)
+dic1={"Nome":"Andre", "Sobrenome":"Alves"}
+jteste = [{
+            "Nome":"Marcos",
+            "Sobrenome":"Patrone"
+          },
+          {
+            "nome":"Maria",
+            "Sobrenome":"Carmo"
+}]
+
+print(jteste[0])
+
