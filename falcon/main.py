a5d314533d8959ae331da770d4789ff71f373d46
André Luis Alves <andre.alves@sixsigma.com.br>
2020-08-10 00:29:54 -0300

    new models

---
+++ falcon/main.py
@@ -0,0 +1,26 @@
+# sample.py
+from wsgiref.simple_server import make_server
+import falcon
+
+class QuoteResource:
+    def on_get(self, req, resp):
+        """Handles GET requests"""
+        quote = {
+            'quote': (
+                "I've always been more interested in "
+                "the future than in the past."
+            ),
+            'author': 'Grace Hopper'
+        }
+
+        resp.media = quote
+
+api = falcon.API()
+api.add_route('/quote', QuoteResource())
+
+if __name__ == '__main__':
+    with make_server('', 8000, api) as httpd:
+        print('Serving on port 8000...')
+
+        # Serve until process is killed
+        httpd.serve_forever()
